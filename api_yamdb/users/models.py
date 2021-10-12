88cecc03ac7167fe61fec5082e70c8da0c927cca
selectcoma <selectkoma@gmail.com>
2021-10-12 12:28:20 +0200

    добавил users в список приложений

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -1,9 +1,63 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+import uuid
+
+ADMIN = 'admin'
+MODERATOR = 'moderator'
+USER = 'user'
+
 
 class User(AbstractUser):
+
+    USER_TYPES = [
+            (ADMIN, 'Администратор'),
+            (MODERATOR, 'Модератор'),
+            (USER, 'Пользователь'),
+        ]
+
+    first_name = models.CharField(
+        max_length=150,
+        verbose_name='Имя',
+        blank=True
+    )
+    last_name = models.CharField(
+        max_length=150,
+        verbose_name='Фамилия',
+        blank=True
+    )
+    email = models.EmailField(
+        verbose_name='Адрес электронной почты',
+        unique=True,
+    )
+
     bio = models.TextField(
-        'Биография',
+        verbose_name='Описание',
+        max_length=500,
         blank=True,
     )
+
+    user_type = models.CharField(
+        max_length=10,
+        verbose_name='Тип пользователя',
+        choices=USER_TYPES,
+        default=USER,
+    )
+
+    confirmation_code = models.UUIDField(
+        verbose_name='Код подтверждения',
+        default=uuid.uuid4,
+        editable=False,
+    )
+
+    USERNAME_FIELD = 'username'
+    EMAIL_FIELD = 'email'
+    REQUIRED_FIELDS = ['email', ]
+
+    def __str__(self):
+        return self.email
+
+    class Meta:
+        ordering = ('username',)
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
