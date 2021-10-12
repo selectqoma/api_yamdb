91e2481623816bceb5e0f5f190c9a1e4d26d34ad
selectcoma <selectkoma@gmail.com>
2021-10-12 18:53:07 +0200

    Coздан класс UserManager

---
+++ api_yamdb/users/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+
+from .models import User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'first_name', 'last_name', 'email', 'role',)
+    search_fields = ('username', 'first_name', 'last_name', 'email', )
+    empty_value_display = '-пусто-'
---
+++ api_yamdb/users/models.py
@@ -0,0 +1,107 @@
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.db import models
+
+import uuid
+
+ADMIN = 'admin'
+MODERATOR = 'moderator'
+USER = 'user'
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, username, **extra_fields):
+        if not email:
+            raise ValueError('Введите адрес электронной почты')
+        if not username:
+            raise ValueError('Введите имя пользователя')
+        user = self.model(
+            email=self.normalize_email(email),
+            username=username,
+            **extra_fields
+        )
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, username, **extra_fields):
+        if not email:
+            raise ValueError('Введите адрес электронной почты')
+        if not username:
+            raise ValueError('Введите имя пользователя')
+        user = self.model(
+            email=self.normalize_email(email),
+            username=username,
+            role='admin',
+            **extra_fields
+        )
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractUser):
+
+    USER_ROLES = [
+            (ADMIN, 'Администратор'),
+            (MODERATOR, 'Модератор'),
+            (USER, 'Пользователь'),
+        ]
+
+    email = models.EmailField(
+        max_length=254,
+        verbose_name='Адрес электронной почты',
+        unique=True,
+        blank=False,
+        null=False
+    )
+
+    first_name = models.CharField(
+        max_length=150,
+        verbose_name='Имя',
+        blank=True,
+        null=True
+    )
+    last_name = models.CharField(
+        max_length=150,
+        verbose_name='Фамилия',
+        blank=True,
+        null=True
+    )
+
+    bio = models.TextField(
+        verbose_name='Описание',
+        max_length=500,
+        blank=True,
+    )
+
+    role = models.CharField(
+        max_length=10,
+        verbose_name='Тип пользователя',
+        choices=USER_ROLES,
+        default=USER,
+    )
+
+    confirmation_code = models.UUIDField(
+        verbose_name='Код подтверждения',
+        default=uuid.uuid4,
+        editable=False,
+    )
+
+    def is_moderator(self):
+        return self.role == 'moderator'
+
+    def is_admin(self):
+        return self.role == 'admin'
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email',]
+
+    def __str__(self):
+        return self.username
+
+    class Meta:
+        ordering = ('username',)
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
