7405c90adf4770d5f749c44d60096e674f2c2dfa
selectcoma <selectkoma@gmail.com>
2021-10-13 10:53:50 +0200

    JWT токен, вьюхи и сериализаторы с ним связанны

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -4,3 +4,19 @@ from rest_framework import serializers
 class SendCodeSerializer(serializers.Serializer):
     email = serializers.EmailField(required=True)
     username = serializers.CharField(required=True)
+
+
+class LogInSerializer(serializers.Serializer):
+    username = serializers.CharField(required=True)
+    confirmation_code = serializers.CharField(required=True)
+
+
+class UserSerializer(serializers.Serializer):
+    class Meta:
+        fields = ['username',
+                  'email',
+                  'bio',
+                  'first_name',
+                  'last_name',
+                  'role'
+                  ]
--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -1,5 +1,7 @@
 import os
+from dotenv import load_dotenv
 
+load_dotenv()
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -22,6 +24,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'api',
     'users',
+    'django_filters'
 ]
 
 MIDDLEWARE = [
@@ -100,3 +103,21 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'),)
 AUTH_USER_MODEL = 'users.User'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = os.getenv('EMAIL')
+EMAIL_HOST_PASSWORD = os.getenv('PASSWORD')
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication'
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny'
+    ],
+    'DEFAULT_FILTER_BACKENDS': [
+            'django_filters.rest_framework.DjangoFilterBackend',
+        ],
+}
