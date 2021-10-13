760818257177866a4b2c503a239e002eb583696a
Artem <lukart80@yandex.ru>
2021-10-13 10:34:23 +0300

    Создана админка для моделей Category, Genre, Title

--- api_yamdb/api/admin.py
+++ api_yamdb/api/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Genre, Title, Category
+
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('slug',)
+    search_fields = ('slug',)
+
+
+@admin.register(Title)
+class TitleAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'genre', 'year')
+    search_fields = ('name', 'category', 'genre', 'year')
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('slug', 'name')
+    search_fields = ('slug', 'name')
