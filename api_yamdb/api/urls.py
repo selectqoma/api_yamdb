e1f48086825ff1a0a4ac99fae97a6b976026f1fa
selectcoma <selectkoma@gmail.com>
2021-10-13 11:40:25 +0200

    Поправил урлы

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import send_code, get_token, UserInfo, AdminViewSet
 
@@ -6,7 +6,8 @@ router = DefaultRouter()
 router.register('users', AdminViewSet)
 
 urlpatterns = [
-    path('v1/auth/signup', send_code, name='get_email_code'),
-    path('v1/auth/token', get_token, name='get_token'),
-    path('v1/users/me', UserInfo.as_view(), name='user_info'),
+    path('v1/auth/signup/', send_code, name='get_email_code'),
+    path('v1/auth/token/', get_token, name='get_token'),
+    path('v1/users/me/', UserInfo.as_view(), name='user_info'),
+    path('v1/', include(router.urls))
 ]
