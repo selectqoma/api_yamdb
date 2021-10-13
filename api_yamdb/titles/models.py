4327bb0f62c3e1ad3724f25be7b5c9596d59a77a
Baranov Ivan <iwanbarano@gmail.com>
2021-10-14 02:55:34 +0300

    corrected models

--- api_yamdb/titles/models.py
+++ api_yamdb/titles/models.py
@@ -1,7 +1,7 @@
 import datetime as dt
 
 from django.db import models
-from django.core.validators import MaxValueValidator, MaxLengthValidator
+from django.core.validators import MaxValueValidator
 from django.contrib.auth import get_user_model
 
 
@@ -11,11 +11,11 @@ User = get_user_model()
 class Category(models.Model):
     """Модель для категории."""
     name = models.CharField(
-        'Название',
+        verbose_name='Название',
         max_length=50
     )
     slug = models.SlugField(
-        'Slug',
+        verbose_name='Slug',
         unique=True
     )
 
@@ -28,8 +28,12 @@ class Category(models.Model):
 
 class Genre(models.Model):
     """Модель для жанров."""
+    name = models.CharField(
+        verbose_name='Название',
+        max_length=50
+    )
     slug = models.SlugField(
-        'Slug',
+        verbose_name='Slug',
         unique=True
     )
 
@@ -54,7 +58,7 @@ class Title(models.Model):
         related_name='titles',
         null=True
     )
-    name = models.CharField('Название', max_length=50)
+    name = models.CharField(verbose_name='Название', max_length=50)
     year = models.PositiveIntegerField(
         validators=[MaxValueValidator(
             dt.datetime.today().year, message='Неверная дата'), ]
@@ -65,72 +69,3 @@ class Title(models.Model):
 
     def __str__(self):
         return self.name
-
-
-class Review(models.Model):
-    """Модлель для обзоров."""
-    SCORE_CHOICES = [
-        (i, i) for i in range(1, 11)
-    ]
-    title = models.ForeignKey(
-        Title,
-        on_delete=models.CASCADE,
-        related_name='reviews',
-        verbose_name='Тайтл'
-
-    )
-    genre = models.ForeignKey(
-        Genre,
-        on_delete=models.CASCADE,
-        related_name='reviews',
-        verbose_name='Жанр'
-    )
-    author = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE,
-        related_name='reviews',
-        verbose_name='Автор',
-    )
-    text = models.TextField(
-        'Текст отзыва',
-        validators=[MaxLengthValidator(90000, message='Превышен лимит знаков')]
-    )
-    score = models.PositiveIntegerField(
-        choices=SCORE_CHOICES,
-        verbose_name='оценка'
-    )
-    pub_date = models.DateField(auto_now_add=True)
-
-    class Meta:
-        ordering = ('-pub_date',)
-
-    def __str__(self):
-        return self.text[:50]
-
-
-class Comment(models.Model):
-    """Модель для комментариев."""
-    review = models.ForeignKey(
-        Review,
-        verbose_name='Обзор',
-        on_delete=models.CASCADE,
-        related_name='comments'
-    )
-    author = models.ForeignKey(
-        User,
-        verbose_name='Автор',
-        related_name='comments',
-        on_delete=models.CASCADE,
-
-    )
-    text = models.TextField(
-        'Текст комментария',
-        validators=[MaxLengthValidator(2000, message='Превышен лимит знаков')]
-    )
-    pub_date = models.DateField(auto_now_add=True)
-
-    class Meta:
-        ordering = ('-pub_date',)
-
-    def __str__(self):
-        return self.text[:50]
