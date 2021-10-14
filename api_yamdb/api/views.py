4a8dbfa80c3f5cb126c38399f435b3c5ee44b1b6
Artem <lukart80@yandex.ru>
2021-10-14 09:54:15 +0300

    Создал view-set для Reviews

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -4,13 +4,18 @@ from .serializers import (
 from users.models import User
 from rest_framework.decorators import api_view, action
 from rest_framework.response import Response
+
 from django.core.mail import send_mail
 from rest_framework import status, viewsets, filters
 from rest_framework.views import APIView
 import uuid
 from django.shortcuts import get_object_or_404
 from rest_framework_simplejwt.tokens import RefreshToken
-from .permissions import IsAdmin
+from .permissions import IsAdmin, IsModerator, IsAuthor, \
+    IsAuthenticatedOrReadOnly
+from .serializers import ReviewSerializer
+
+from titles.models import Title
 
 
 @api_view(['POST'])
@@ -52,7 +57,8 @@ def get_token(request):
         user = get_object_or_404(User, username=username)
         if confirmation_code == str(user.confirmation_code):
             token = RefreshToken.for_user(user)
-            return Response({f'Ваш токен: {token}'}, status=status.HTTP_200_OK)
+            return Response({f'Ваш токен: {token.access_token}'},
+                            status=status.HTTP_200_OK)
         return Response('Неправильный код',
                         status=status.HTTP_400_BAD_REQUEST)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
@@ -63,7 +69,7 @@ class AdminViewSet(viewsets.ModelViewSet):
     serializer_class = AdminUserSerializer
     filter_fields = ('role',)
     lookup_field = 'username'
-    permission_classes = (IsAdmin, )
+    permission_classes = (IsAdmin,)
     filter_backends = [filters.SearchFilter]
     search_fields = ['username', 'email']
 
@@ -74,7 +80,8 @@ class UserInfo(APIView):
             user = get_object_or_404(User, username=request.user.username)
             serializer = UserSerializer(user)
             return Response(serializer.data)
-        return Response('Вы не авторизированы', status=status.HTTP_401_UNAUTHORIZED)
+        return Response('Вы не авторизированы',
+                        status=status.HTTP_401_UNAUTHORIZED)
 
     def patch(self, request):
         if request.user.is_authenticated:
@@ -83,5 +90,29 @@ class UserInfo(APIView):
             if serializer.is_valid():
                 serializer.save()
                 return Response(serializer.data, status=status.HTTP_200_OK)
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        return Response('Вы не авторизированы', status=status.HTTP_401_UNAUTHORIZED)
+            return Response(serializer.errors,
+                            status=status.HTTP_400_BAD_REQUEST)
+        return Response('Вы не авторизированы',
+                        status=status.HTTP_401_UNAUTHORIZED)
+
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    serializer_class = ReviewSerializer
+    permission_classes = [
+        IsAuthenticatedOrReadOnly,
+        IsAuthor | IsModerator | IsAdmin
+    ]
+
+    def get_queryset(self):
+        title_id = self.kwargs.get('title_id')
+        title = get_object_or_404(Title, pk=title_id)
+        return title.reviews.all()
+
+    def perform_create(self, serializer):
+        title = get_object_or_404(Title, pk=self.kwargs.get('title_id'))
+        author = self.request.user
+        serializer.save(
+            author=author,
+            title=title
+
+        )
