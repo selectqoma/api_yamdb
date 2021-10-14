7cf30a9edeadfa28faa19da8e0c4d7d21f550065
Artem <lukart80@yandex.ru>
2021-10-14 09:54:28 +0300

    Добавил urls для review

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import send_code, get_token, AdminViewSet, UserInfo
+from .views import send_code, get_token, AdminViewSet, UserInfo, ReviewViewSet
 
 router = DefaultRouter()
 router.register('users', AdminViewSet)
+router.register(
+    r'titles/(?P<title_id>\d+)/reviews',
+    ReviewViewSet,
+    basename='reviews'
+
+)
 
 urlpatterns = [
     path('v1/auth/signup/', send_code, name='get_email_code'),
