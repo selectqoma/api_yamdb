fe3b5060a69453ed23f458269de0dfef53c1a23c
selectcoma <selectkoma@gmail.com>
2021-10-17 09:47:21 +0200

    Тесты - 1 последний остался

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -11,20 +11,21 @@ router = DefaultRouter()
 router.register('users', AdminViewSet)
 
 router.register(
+    r'titles/(?P<title_id>\d+)/reviews',
+    ReviewViewSet,
+    basename='reviews'
+)
+
+router.register(
     r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
     CommentViewSet,
     basename='comments'
 
 )
+
 router.register('genres', GenreViewSet, basename='genres')
 router.register('categories', CategoryViewSet, basename='categories')
 router.register('titles', TitleViewSet, basename='titles')
-
-router.register(
-    r'titles/(?P<title_id>\d+)/reviews',
-    ReviewViewSet,
-    basename='reviews'
-)
 
 urlpatterns = [
     path('v1/auth/signup/', send_code, name='get_email_code'),
